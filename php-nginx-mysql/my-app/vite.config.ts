501d6a29f9af6e75167cd14ea6480f2fba418b50
taktiks2 <deathproof.lee@gmail.com>
2025-11-06 22:09:28 +0900

    feat: ホットリロードが効くようにする変更

--- php-nginx-mysql/my-app/vite.config.ts
+++ php-nginx-mysql/my-app/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.tsx"],
-            refresh: true,
+            refresh: ["resources/views/**", "resources/js/**"],
         }),
         tailwindcss(),
         react(),
